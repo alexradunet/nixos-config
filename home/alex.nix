02032316e06aced885fef1b6a6e920a067710d6a
alex <dev@alexradu.net>
2026-04-19 15:51:32 +0300

    add how github ssh should work

--- home/alex.nix
+++ home/alex.nix
@@ -72,4 +72,14 @@
     enable = true;
     enableZshIntegration = true;
   };
+
+  programs.ssh = {
+    enable = true;
+    matchBlocks = {
+      "github.com" = {
+        user = "git";
+        identityFile = "~/.ssh/id_ed25519";
+      };
+    };
+  };
 }
