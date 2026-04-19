fcdd9ce4469d780e1e3912d868659ad5f4eac064
alex <dev@alexradu.net>
2026-04-19 15:38:07 +0300

    make it bootable

--- hosts/pad-nixos/default.nix
+++ hosts/pad-nixos/default.nix
@@ -11,6 +11,9 @@
 
   networking.hostName = "pad-nixos";
 
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
   networking.networkmanager.enable = true;
 
   nixpkgs.config.allowUnfree = true;
