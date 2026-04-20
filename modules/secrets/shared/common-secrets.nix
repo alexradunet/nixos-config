d06413ebde88ab24e954185e548dc42d16348477
alex <dev@alexradu.net>
2026-04-20 19:57:58 +0300

    asdf

--- modules/secrets/shared/common-secrets.nix
+++ modules/secrets/shared/common-secrets.nix
@@ -2,14 +2,6 @@
   secretFile = ../../../secrets/common.yaml;
 in
   lib.mkIf (builtins.pathExists secretFile) {
-    sops.secrets.github-token = {
-      sopsFile = secretFile;
-      key = "github/token";
-      owner = "alex";
-      group = "users";
-      mode = "0400";
-    };
-
     sops.secrets.cortecs-api-key = {
       sopsFile = secretFile;
       key = "cortecs/apiKey";
