50e9901e145b0d351464430622fd65651331daeb
alex <dev@alexradu.net>
2026-04-21 21:01:01 +0300

    add nodejs

--- modules/features/home/packages/module.nix
+++ modules/features/home/packages/module.nix
@@ -1,4 +1,11 @@
 {pkgs, ...}: {
+  # Set a writable npm global prefix so `npm install -g` (used by `pi install`)
+  # doesn't try to write into the read-only nix store.
+  home.file.".npmrc".text = ''
+    prefix = /home/alex/.npm-global
+  '';
+  home.sessionPath = [ "/home/alex/.npm-global/bin" ];
+
   home.packages = with pkgs; [
     ripgrep
     fd
@@ -17,5 +24,6 @@
     procs # colourised `ps` with tree view and search
     bottom # GPU-aware `htop` replacement with charts
     qmd # hybrid BM25/vector search for markdown
+    nodejs # node + npm, needed for `pi install npm:...`
   ];
 }
